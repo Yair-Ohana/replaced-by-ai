1aebac89840d720c395aea5837399e1e2a65e6f4
Yair Ohana <yairohanap@gmail.com>
2022-12-30 12:15:53 +0200

    feat: updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 function App() {
+  const LinkBox = () => {
+    return (
+      <div
+        onClick={() => {
+          window.location.href = "https://www.willaireplaceme.com";
+        }}
+        className="box-container"
+      >
+        <span className="small-icon">🤖</span>
+        <span>willaireplaceme.com</span>
+      </div>
+    );
+  };
+
   return (
     <div className="container">
       <span className="icon">🤖</span>
-      <h1>Fuck Robots.</h1>
+      <h1>F*ck Robots.</h1>
+      <LinkBox />
     </div>
   );
 }
